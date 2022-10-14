5a0e447a632fab51d7f19a16bdcc21a99bbf9d76
Akhmed Musaurov <MUSAUROV-A@MAiL.RU>
2022-10-14 17:32:45 +0300

    3

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Массив из 8 чисел: ");
+void PrintArray(int[]array)
+{
+    int count = array.Length;
+    for (int a = 1; a < count; a++)
+    {
+        Console.Write($"{array[a]}  ");
+    }
+}
+int [] mass = new int[9];
+Random rand = new Random();
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = rand.Next(1, 100);
+}
+PrintArray(mass);
